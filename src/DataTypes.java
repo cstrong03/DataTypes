cc0f370da0f10cdbd89e438975ed71065d82880f
Majestic <charissestrong@gmail.com>
2021-10-28 13:20:32 -0400

    Datatypes file

---
+++ src/DataTypes.java
@@ -0,0 +1,17 @@
+public class DataTypes {
+    //Primitive Types - are the most basic data types. There are eight of them. (Boolean, btye, char, short, int, long, float, and double)
+    //Int = integer( and whole number from 0 to infinity)
+
+    public static void main(String[] args){
+        System.out.println("Hello Charisse");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber *2;
+        int mySumOfNumber = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(mySumOfNumber);
+        int myLastOne = 1000 - mySumOfNumber;
+        System.out.println(myLastOne);
+    }
+
+}
